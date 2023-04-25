3a1cee7144419d5be025214d9544d673ba2b90da
trodevel <trodevel@gmail.com>
2023-04-25 21:11:59 +0200

    removed leading zeros

--- resources/generate_currencies_py.sh
+++ resources/generate_currencies_py.sh
@@ -6,4 +6,4 @@ FL=../currencies.py
 
 echo "from enum import Enum" > $FL
 echo "class Currency(int, Enum):" >> $FL
-awk -F"\t" '{printf "    %s = %s # %s\n", $1, $2, $4; }' currencies.csv >> $FL
+awk -F"\t" '{printf "    %s = %s # %s\n", $1, $2, $4; }' currencies.csv | sed "s/= [0]*/= /">> $FL
