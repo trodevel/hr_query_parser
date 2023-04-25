0e03351eacd1121c49c72e5011164a35b7b677a6
trodevel <trodevel@gmail.com>
2023-04-25 20:43:10 +0200

    syntax error fix

--- resources/generate_languages_py.sh
+++ resources/generate_languages_py.sh
@@ -4,4 +4,4 @@ FL=../languages.py
 
 echo "from enum import Enum" > $FL
 echo "class Language(int, Enum):" >> $FL
-sed -e "s/, /_/" -e "s/ /_/" -e "s/\-/_/" languages.csv | awk -F";" '{printf "    %s = %s\n", $2, $1; }' >> $FL
+sed -e "s/, /_/" -e "s/ /_/g" -e "s/\-/_/g" languages.csv | awk -F";" '{printf "    %s = %s\n", $2, $1; }' >> $FL
