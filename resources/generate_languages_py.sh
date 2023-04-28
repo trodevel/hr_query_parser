77816608bd466ba67af1bd1568d3c36bdc4fa2b5
trodevel <trodevel@gmail.com>
2023-04-28 03:17:16 +0200

    re-enumerated languages

--- resources/generate_languages_py.sh
+++ resources/generate_languages_py.sh
@@ -4,4 +4,4 @@ FL=../languages.py
 
 echo "from enum import Enum" > $FL
 echo "class Language(int, Enum):" >> $FL
-sed -e "s/, /_/" -e "s/ /_/g" -e "s/\-/_/g" languages.csv | awk -F";" '{printf "    %s = %s\n", $2, $1; }' >> $FL
+tail -n +2 languages.csv | tr "\t" ";" | awk -F";" '{ printf "%s;%s\n", $2, $1; }' | nl -s";" -nln -w1 | awk -F";" '{printf "    %s = %s # %s\n", $2, $1, $3; }' >> $FL
