5a2140fc2d9cda3d6bec60230729600bd4f2121f
trodevel <trodevel@gmail.com>
2023-05-02 19:14:57 +0200

    TOOLS: simplified

--- tools/generate_languages_py.sh
+++ tools/generate_languages_py.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+INP=../resources/languages.csv
 FL=../languages.py
 
 echo "from enum import Enum" > $FL
 echo "class Language(int, Enum):" >> $FL
-tail -n +2 ../resources/languages.csv | tr "\t" ";" | awk -F";" '{ printf "%s;%s\n", $2, $1; }' | nl -s";" -nln -w1 | grep -vw "as\|or\|is" | awk -F";" '{printf "    %s = %s # %s\n", $2, $1, $3; }' >> $FL
+tail -n +2 $INP | tr "\t" ";" | awk -F";" '{ printf "%s;%s\n", $2, $1; }' | nl -s";" -nln -w1 | grep -vw "as\|or\|is" | awk -F";" '{printf "    %s = %s # %s\n", $2, $1, $3; }' >> $FL
