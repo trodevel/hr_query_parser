8285dc93c0680ba5210e4e492c701277527ea3b2
trodevel <trodevel@gmail.com>
2023-06-27 17:28:40 +0200

    tools: added checkout-externals.sh

---
+++ checkout-externals.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+svn export https://github.com/trodevel/languages/tags/1.0.0/python externals/languages
+svn export https://github.com/trodevel/currencies/tags/1.0.0/python externals/currencies
+svn export https://github.com/trodevel/hr_locations/tags/1.0.0/resources assets/locations
+svn export https://github.com/trodevel/hr_skills/tags/1.0.0/resources assets/skills
+svn export https://github.com/trodevel/hr_specializations/tags/2.0.0/resources assets/specializations
+svn export https://github.com/trodevel/hr_job_formats/tags/1.0.0/resources assets/job_formats
+svn export https://github.com/trodevel/hr_qualifications/tags/1.0.0/resources assets/qualifications
